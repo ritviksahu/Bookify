e9f266afac4ab1ac42bc03b345e5a63864ab18b9
ritviksahu <ritvik15sahu@gmail.com>
2024-04-14 12:36:15 +0530

    Adding description of the project

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Bookify
+
+"Bookify" is a web application developed using JavaScript (JS) and Node.js, with Express.js framework handling the server-side logic.
+<h3>Technology Stack:</h3>
+
+1. Developed using JavaScript (JS) and Node.js.
+2. Utilizes Express.js for server-side development.
+
+<h3>RESTful API Architecture:</h3>
+
+1. Follows RESTful principles for API design.
+2. Provides endpoints for functionalities such as fetching book details, searching by title, and reading reviews.
+
+<h3>Express Router for Route Management:</h3>
+
+1. Organizes backend logic using Express Router.
+2. Ensures clear separation of concerns and maintainable codebase.
+
+<h3>Asynchronous Programming:</h3>
+
+1. Uses Promises and callback functions to manage asynchronous operations.
+2. Enables efficient execution of tasks like retrieving book details and processing user requests.
+
+<h3>Client-Side JavaScript:</h3>
+
+1. Enhances the user interface with dynamic interactions.
+2. Facilitates a seamless browsing experience for users.
+3. Designed to be responsive, catering to users across different devices and screen sizes.
+
+<h3>User-Friendly Platform:</h3>
+
+1. Provides a user-friendly platform for book enthusiasts.
+2. Allows users to explore, discover, and engage with literature in a digital environment.
--- router/general.js
+++ router/general.js
@@ -24,19 +24,24 @@ public_users.post("/register", (req,res) => {
     res.status(201).json({ message: 'User registered successfully' });
 });
 
-//Task1
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
+//Task1
     //res.send(JSON.stringify(books,null,11));
+//Task10
     res.json(books);
 });
 
 
-//Task2
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+//Task2
     // const isbn = req.params.isbn;
     // res.send(books[isbn]);
+
+//Task11
     const isbn = req.params.isbn;
     if (!isbn) {
         return res.status(400).json({ message: 'ISBN is required' });
@@ -68,9 +73,11 @@ function getBookDetailsByISBN(isbn) {
 });
 }
   
-//Task3
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+    
+//Task3
     // const author = req.params.author;
     // const booksByAuthor = [];
     
@@ -90,7 +97,7 @@ public_users.get('/author/:author',function (req, res) {
     // } else {
     //     res.status(404).json({ message: 'No books found for the author' });
     // }
-
+//Task12
     const author = req.params.author;
     if (!author) {
         return res.status(400).json({ message: 'Author is required' });
@@ -125,9 +132,11 @@ function getBookDetailsByAuthor(author) {
     });
 }
 
-//Task4
+
 // Get all books details based on title
 public_users.get('/title/:title',function (req, res) {
+    
+//Task4    
     // const title = req.params.title;
     // const booksByTitle = [];
     
@@ -147,6 +156,7 @@ public_users.get('/title/:title',function (req, res) {
     // } else {
     //     res.status(404).json({ message: 'No books found for the author' });
     // }
+//Task13
     const title = req.params.title;
     if (!title) {
         return res.status(400).json({ message: 'Title is required' });
